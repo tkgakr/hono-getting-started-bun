5d2abe23f4075801479e368c42a274f0293d7329
Akira Takagi <tkgakrwork@gmail.com>
2026-06-21 16:58:14 +0900

    URLの/static/* を フォルダ./statics にマップ

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
 
-app.use('/static/*', serveStatic({ root: './' }))
 app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
 app.get('/', (c) => c.text('You can access: /static/hello.txt'))
-app.get('*', serveStatic({ path: './static/fallback.txt' }))
+app.get(
+  '/static/*',
+  serveStatic({
+    root: './',
+    rewriteRequestPath: (path) => path.replace(/^\/static/, '/statics'),
+  })
+)
+app.get('*', serveStatic({ path: './statics/fallback.txt' }))
 
 export default {
   port: 3001,
