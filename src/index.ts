99c36e4e60d637d7adbd96044d8c826fcaecf3fe
Akira Takagi <tkgakrwork@gmail.com>
2026-06-21 16:46:49 +0900

    静的ファイルの提供

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use('/static/*', serveStatic({ root: './' }))
+app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
+app.get('/', (c) => c.text('You can access: /static/hello.txt'))
+app.get('*', serveStatic({ path: './static/fallback.txt' }))
 
 export default {
   port: 3001,
